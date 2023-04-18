5ddf39dfe7e84211eb1dd437f9f7ac8ea2a87489
EC2 Default User <ec2-user@ip-172-31-5-102.ap-northeast-1.compute.internal>
2023-04-18 02:50:16 +0000

    [Update] 3-12 RSpecエラー修正前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,55 +1,53 @@
 class BooksController < ApplicationController
-  # データの新規作成フォームを表示アクション
+  # データの新規作成フォームを表示するアクション
   def new
     @book = Book.new
   end
-  
-  # データを追加（保存）アクション
+
+  # データを追加（保存）するアクション
   def create
-    # book = Book.new(book_params)
-    # book.save
-    # flash[:notice] = "Book was successfully created."
-    # redirect_to book_path(book.id)
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       render :new
     end
   end
 
-  # データを追加（保存）アクション
+  # データを追加（保存）するアクション
   def index
     @books = Book.all
   end
 
-  # データの内容（詳細）を表示アクション
+  # データの内容（詳細）を表示するアクション
   def show
     @book = Book.find(params[:id])
   end
 
-  # データを更新するためのフォームを表示アクション
+  # データを更新するためのフォームを表示するアクション
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  # データを更新するアクション
   def update
-    # book = Book.find(params[:id])
-    # book.update(book_params)
-    # redirect_to book_path(book.id)
     @book = Book.find(params[:id])
-    if @book.save
-      flash[:notice] = "Book was successfully update."
-      redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:success] = "Book was successfully update."
+      redirect_to book_path(book)
     else
-      render :edit
+      render :new
     end
   end
-  
+
+  # データを削除するアクション
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book_path(@book.id)
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
